9d37369300bf1b5e9651dd59f80ded1ae4ce3199
jfriedlein <58786257+jfriedlein@users.noreply.github.com>
2020-01-01 11:37:51 +0100

    added the second derivative (preliminary version)

--- docs/mainpage-framework.h
+++ docs/mainpage-framework.h
@@ -18,18 +18,36 @@ for instance with data types double as \n
 \f[ 1.0 + 1.0 \rightarrow 2.0 \f]
 your results is just a double number \f$ c \f$ that contains the value \f$ 2 \f$. \n
 
-Using Sacado, on the other hand, the variable \f$ c \f$ is now of, for example, data type @code Sacado::Fad::DFad<double> @endcode.
-As a result, \f$ c \f$ now contains not just the number \f$ 2 \f$, but also all the derivatives of \f$ c \f$
+Using Sacado, on the other hand, the variable \f$ c_{fad} \f$ is now of, for example, data type
+@code
+	Sacado::Fad::DFad<double> c_fad;
+@endcode
+As a result, \f$ c_{fad} \f$ now contains not just the number \f$ 2 \f$, but also all the derivatives of \f$ c_{fad} \f$
 with respect to the previously defined degrees of freedom (set via command *.diff(*)). \n
 The following figure tries to visualize this:
 \image html Sacado_data-type.png
-@todo redo this figure
+@todo update this figure or add another one for second derivatives
+@todo add another less general figure with c, a and b and explain what is meant by point p
 
 If you right away want to use Sacado, then you might skip the first examples and jump to Ex3B.
-There we show how to use the "Sacado_Wrapper" that does everything from Ex2 and Ex3 in just a view lines of code.
+There we show how to use the "Sacado_Wrapper" that does everything from Ex2 and Ex3 in just a view lines of code. This does not mean that
+the here shown approach is the fastest or most efficient, it is just simple and easy to use.
 
 Furthermore, if you, for instance, compute problems with two-fields (e.g. displacement and scalar damage) and you need
 tangents with respect to both a tensor (e.g. strain tensor) and a scalar (e.g. damage variable), you can use the Sacado_Wrapper as shown in Ex4.
+
+Some more basics: \n
+One can access the double value of \f$ c_{fad} \f$ with the Sacado command *.val():
+@code
+	double c_value = c_fad.val();
+@endcode
+The derivatives of \f$ c_{fad} \f$ can be accessed with the command *.dx():
+@code
+	double d_c_d_a = c_fad.dx(0);
+	double c_c_d_b = c_fad.dx(1);
+@endcode
+The arguments of \a dx, namely 0 and 1 are the numbers corresponding to the dof that belong to \a a and \a b. More details on how to set this up
+and use it, are given in example Ex1.
 
 Some resources/links: \n
 @todo link the Sacado and DII pages
@@ -38,6 +56,7 @@ The here shown examples shall solely show how Sacado can be applied and give som
 The code is neither elegant nor efficient, but it works. A more user-friendly version is provided by means of the "Sacado_Wrapper". \n
 @todo add list of files and an overview
 @todo explain how to use the Wrapper (download the file Sacado_Wrapper.h, #include, ...)
+@todo Check if factor 0.5 is also necessary for d_sigma / d_phi
 
 @note This documentation and code only protocol my first steps with Sacado. They are not guaranteed to be correct neither are they verified.
 Any comments, criticism, corrections, feedback, improvements, ... are very well welcomed.
--- docs/transform_code2Mainpage - Sacado-Testing.py
+++ docs/transform_code2Mainpage - Sacado-Testing.py
@@ -3,7 +3,7 @@
 """
 Created on Sat Dec 21 18:50:30 2019
 
-@author: johannes
+@author: jfriedlein
 """
 # useful commands to clear the workspace and clear the console (MATLAB: clear,clc) from https://stackoverflow.com/questions/54943710/code-to-clear-console-and-variables-in-spyder
 try:
@@ -13,28 +13,32 @@ try:
 except:
     pass
 
+#
 # Transform normal code into Doxygen mainpage
-dirMainpage = "/home/johannes/DEALII/MA-Code/Sacado-Testing/docs/mainpage-framework.h"
-dirCode = "/home/johannes/DEALII/MA-Code/Sacado-Testing/Sacado_example.cc"
-mergedFile = "/home/johannes/DEALII/MA-Code/Sacado-Testing/mainpage.h"
+#
+    
+# set the paths to ...
+dirMainpage = "./mainpage-framework.h" # ... the framework file containing e.g. the introduction text
+dirCode = "../Sacado_example.cc"  # ... the code that will be placed into the mainpage between the keywords "\code" and "\endcode"
+mergedFile = "../mainpage.h"  # ... the output file containing the text from the framework and the code from the dirCode-file
 
 do_replace = False
-with open(mergedFile, "w") as outputFile:
-    with open(dirMainpage, "r") as mainpageDoc:
+with open(mergedFile, "w") as outputFile: # write into the output file line by line
+    with open(dirMainpage, "r") as mainpageDoc: # read the framework file line by line until ...
         for line in mainpageDoc:
-            if ( "\code" in line ):
-                #outputFile.write(line) # output this found "\code" from the mainpage file
-                # modify the code an write it into the mainpage file
-                code = True
+            if ( "\code" in line ):  # ... you find this keywork, then place the code after this keyword until ...
+                # modify the code an write it into the mainpage file:
+                code = True # True: the current line contains code; False: the current line contains a comment that shall be outputed as normal text
                 firstLine = True
-                #emptyLine = 0
-                with open(dirCode, "r") as codeDoc:
+                with open(dirCode, "r") as codeDoc:  # read the code line by line
                     for lineCode in codeDoc:
+                        # read the first few characters from this line that aren't blank
                         first_nonspace_index = len(lineCode) - len(lineCode.lstrip())
                         first_N_nonspace_letters = lineCode[first_nonspace_index:first_nonspace_index+3]
-                        codeLast = code
+                        codeLast = code # stores whether the last line was code or comment
                         
-                        # instead of /// we indicate a completely commented out line by use no space after //, e.g. "//std::cout ..." is commented out but not a comment like "// This is a comment"
+                        # instead of /// we indicate a completely commented out line by use no space after //,
+                        #  e.g. "//std::cout ..." is commented out but not a comment like "// This is a comment"
                         #if ( first_N_nonspace_letters == "///" ): # this indicates some completely outcommented code, so we keep the comment and don't create a text from this
                         #    lineCode = lineCode[1:len(lineCode)] # on
                         #    code = True
@@ -44,16 +48,17 @@ with open(mergedFile, "w") as outputFile:
                             else:
                                 lineCode = lineCode[(first_nonspace_index+3):len(lineCode)]
                             code = False
-                        elif ( first_N_nonspace_letters == "//\n" ): # when it is an entirely empty comment line (without blank after //)
+                        elif ( first_N_nonspace_letters == "//\n" ): # when it is an entirely empty comment line (without blank after "//")
                             lineCode = "\n"
                             code = False
                         elif ( first_N_nonspace_letters == "" ): # line is an empty line
                             lineCode =" \n"
-                            code = codeLast     # save the type of the last line
-                        else:
+                            code = codeLast     # save the type of the last line and do this until you find code or comment
+                        else: # the line is actually code
                             # lineCode is used unchanged
                             code = True
                          
+                        # At first, we have to either create a code-block for a line containing code or we start with normal text for comment
                         if ( firstLine == True ):
                             if ( code == True ): # if the first line is a code then we have to print the "\code"-command
                                 codeLast = False # to trigger the corresponding "elif" below
@@ -61,24 +66,27 @@ with open(mergedFile, "w") as outputFile:
                                 codeLast = False # so we will just print the line as it is
                             firstLine = False
                                                 
-                        #if ( emptyLine == False ):
-                        if ( code==codeLast ):
+                        if ( code==codeLast ): # the current line is of the same type as the last, so we can remain in the currently active block (either code embraced in \code...\endcode or a text)
                             outputFile.write(lineCode)
-                        elif ( codeLast==False and code==True ):
-                            outputFile.write("\code\n")
+                        elif ( codeLast==False and code==True ): # the text paragraph ended and now we found a line of code ...
+                            outputFile.write("\code\n")  # ... so we start a code block
                             outputFile.write(lineCode)
-                        elif ( codeLast == True and code == False ):
-                            outputFile.write("\endcode\n")
+                        elif ( codeLast == True and code == False ): # our last line was code and now we found a text ...
+                            outputFile.write("\endcode\n") # ... so we close the current code block and start a normal text
                             outputFile.write(lineCode)
                             
                     if ( code == True ): # ensure that the blocks are closed at the end
                         outputFile.write("\endcode\n")
                 do_replace = True
         
-            if ( do_replace == False ):
+            # if we don't add the text from the code into the outputFile,
+            #  we want to continue copying everything from the framework file into the output
+            if ( do_replace == False ): 
                 outputFile.write(line)
         
-            if ( do_replace == True and ("\endcode" in line) ): # when you find the "\endcode"-command in the source mainpage then stop replacing things, but only for the next line because we don't want to add this \endcode-command to the outputFile
+            # ... you find the "\endcode"-command in the source mainpage then stop replacing things,
+            #     but only for the next line because we don't want to add this \endcode-command to the outputFile
+            if ( do_replace == True and ("\endcode" in line) ): 
                 do_replace = False
                 
 
